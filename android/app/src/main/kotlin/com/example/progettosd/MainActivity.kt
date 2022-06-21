debc3f993f88dad79f00c276cc005311c710ae48
Andrea De Luna <andreadeluna@Andreas-MacBook-Pro.local>
2022-06-21 17:39:25 +0200

    Caricamento applicazione

---
+++ android/app/src/main/kotlin/com/example/progettosd/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.progettosd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          brightness: Brightness.dark,
+          primaryColor: Colors.deepPurple,
+          elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.deepPurple)
+              )
+          ),
+          appBarTheme: AppBarTheme(
+            elevation: 0,
+          )
+      ),
+
+    );
+  }
+}
